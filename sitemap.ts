10cc25e180c8e6de0634c9095ae92f999eaa54f6
Muthuvijayan Thakshayani <tmvijay2000@gmail.com>
2026-02-28 15:43:43 +0530

    sitemap

---
+++ sitemap.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://neonsquad.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://neonsquad.vercel.app/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
